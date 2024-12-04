161426914a4946b78c9765565d96bd1decc5bd20
Leehyun-woo <hyun325@deepfine.ai>
2024-12-04 22:49:21 +0900

    feature GameEnd Collider Settings

--- Assets/Scripts/Managers/LanguageManager.cs
+++ Assets/Scripts/Managers/LanguageManager.cs
@@ -11,20 +11,7 @@ namespace MoewMerge.Managers
     {
         public Locales SetLocale(Locales locale)
         {
-            LocalizationSettings.AvailableLocales.Locales.ForEach(locale =>
-                {
-                    Debug.Log($"LHWCode{locale.Identifier.Code}");
-                    Debug.Log($"LHWEqualsCode{locale.Identifier.Code.Equals(locale.ToString())}");
-                    Debug.Log(locale.Formatter.ToString().Equals(locale.ToString()));
-                    Debug.Log($"LHW{locale.Formatter}");
-                    Debug.Log(locale.LocaleName);
-                    Debug.Log($"LHW{locale}");
-                    Debug.Log($"LHW{locale.CustomFormatterCode}");
-                });
-            ;
-            Debug.Log($"LHWLocale{locale}");
             Locale createLocale = Locale.CreateLocale(locale.ToString());
-            //Locale findLocale = LocalizationSettings.AvailableLocales.Locales.Select(locale => locale).Where(locale => locale.Identifier.Code.Equals(locale.ToString())).First();
             LocalizationSettings.SelectedLocale = createLocale;
             return GameManager.Instance.SetLocale(locale);
         }
