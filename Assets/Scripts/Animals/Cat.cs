1ea7f9bb9902a5c31f851d4806d6b3afa281cc29
Leehyun-woo <lolkarma325@gmail.com>
2024-12-08 16:23:28 +0900

    feature Vibrate add

--- Assets/Scripts/Animals/Cat.cs
+++ Assets/Scripts/Animals/Cat.cs
@@ -1,9 +1,7 @@
 using MoewMerge.Managers.Interfaces;
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 namespace MoewMerge.Animals
 {
@@ -45,13 +43,14 @@ namespace MoewMerge.Animals
         // Update is called once per frame
         async void Update()
         {
+            await ReadyDependency();
             if (EventSystem.current.currentSelectedGameObject || !GameManager.IsPlaying())
             {
                 return;
             }
             if (Input.GetMouseButton(0) && !RigidBody.simulated)
             {
-
+                Handheld.Vibrate();
                 Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 minBorderX = minBorderX.Equals(float.MinValue) ? GameManager.GetLeftEndPosition(Vector2.one * RectTransform.sizeDelta * transform.localScale / 2f) : minBorderX;
                 maxBorderX = maxBorderX.Equals(float.MinValue) ? GameManager.GetRightEndPosition(Vector2.one * RectTransform.sizeDelta * transform.localScale / 2f) : maxBorderX;
@@ -78,6 +77,13 @@ namespace MoewMerge.Animals
             GameManager.NextCats();
             GameManager.AddGameScore(CatMerge.CatLevel);
             this.enabled = false;
+        }
+        private async Task ReadyDependency()
+        {
+            while(GameManager is null)
+            {
+                await Task.Yield();
+            }
         }
         // ³Ø½ºÆ® UI
         public void Ready()
